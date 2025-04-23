0d76a056e276826af040eb528bceb139e5bc2a93
Leo Guinan <leo.guinan@gmail.com>
2025-04-23 18:58:59 -0400

    fixing database

--- config/database.js
+++ config/database.js
@@ -1,14 +1,7 @@
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
+    connection: env('DATABASE_URL'),
     debug: false,
   },
 }); 
